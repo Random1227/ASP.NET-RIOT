ee14d9112150648660d90ddb6a5b0415bc20b1dd
Random1227 <61437210+Random1227@users.noreply.github.com>
2021-05-08 15:24:12 -0400

    Add files via upload

---
+++ Data/LeagueMatchHistoryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LeagueMatchHistory.Models;
+
+namespace LeagueMatchHistory.Data
+{
+    public class LeagueMatchHistoryContext : DbContext
+    {
+        public LeagueMatchHistoryContext (DbContextOptions<LeagueMatchHistoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeagueMatchHistory.Models.Summoner> Summoner { get; set; }
+    }
+}
---
+++ Migrations/20210508162215_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LeagueMatchHistory.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Summoner",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SummonerId = table.Column<string>(type: "varchar(64)", nullable: true),
+                    AccountId = table.Column<string>(type: "varchar(64)", nullable: true),
+                    PuuId = table.Column<string>(type: "varchar(64)", nullable: true),
+                    SummonerName = table.Column<string>(type: "varchar(64)", nullable: true),
+                    RevisionDate = table.Column<string>(type: "varchar(64)", nullable: false),
+                    SummonerLevel = table.Column<string>(type: "varchar(64)", nullable: false),
+                    LeagueId = table.Column<string>(type: "varchar(64)", nullable: true),
+                    QueueType = table.Column<string>(type: "varchar(64)", nullable: true),
+                    Tier = table.Column<string>(type: "varchar(64)", nullable: true),
+                    Rank = table.Column<string>(type: "varchar(64)", nullable: true),
+                    ProfileIconId = table.Column<string>(type: "varchar(64)", nullable: false),
+                    LeaguePoints = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Wins = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Losses = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Veteran = table.Column<string>(type: "varchar(64)", nullable: false),
+                    Inactive = table.Column<string>(type: "varchar(64)", nullable: false),
+                    FreshBlood = table.Column<string>(type: "varchar(64)", nullable: false),
+                    HotStreak = table.Column<string>(type: "varchar(64)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Summoner", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Summoner");
+        }
+    }
+}
